f090291dcdb024b84836cd425a3179a8efcbb5a1
JuancaSterba <sjcexe@gmail.com>
2025-09-08 16:22:46 -0300

    feat: Initial commit with Spring Boot project structure

---
+++ src/test/java/com/juancasterba/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancasterba.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
